b1ea979cbfee679585f9a13b0964c2f01a964293
Amin Hashemi <43804911+aaminhashemi@users.noreply.github.com>
2024-02-09 12:48:23 +0330

    Create exception_consts.dart

---
+++ lib/utils/exception_consts.dart
@@ -0,0 +1,5 @@
+class Exception_consts{
+  static const String dataFetchError='خطا در دریافت داده ها';
+  static const String incorrectCredentials='اطلاعات وارد شده با داده های ما همخوانی ندارد!';
+  static const String ConnectionError='خطا در برقراری ارتباط، اتصال به اینترنت را بررسی نمایید.';
+}
