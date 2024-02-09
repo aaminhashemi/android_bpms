a6375df3e02a30e36eedf7aa2d56214802e9e960
Amin Hashemi <43804911+aaminhashemi@users.noreply.github.com>
2024-02-09 12:48:02 +0330

    Update auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
+import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   final String baseUrl;
   static const String tokenKey = 'access_token';
+  static const String infoKey = 'user_info';
 
   AuthService(this.baseUrl);
 
@@ -24,14 +26,48 @@ class AuthService {
     return prefs.getString(tokenKey);
   }
 
+  Future<String?> getInfo() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(infoKey);
+  }
+
   Future<void> saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(tokenKey, token);
   }
 
+  Future<void> saveInfo(String userName) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(infoKey, userName);
+  }
+
   Future<void> removeToken() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove(tokenKey);
+  }
+
+  Future<Uint8List?> fetchImageFromServer() async {
+    final token = await this.getToken();
+    try {
+      final response =
+          await http.get(Uri.parse('${baseUrl}/get-profile'), headers: {
+        'Authorization': 'Bearer $token',
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      });
+      print(response);
+
+      if (response.statusCode == 200) {
+        print(response);
+        return response.bodyBytes;
+      } else {
+        print(
+            'Failed to fetch image from server. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching image from server: $e');
+    }
+    return null;
   }
 
   Future<bool> isAccessTokenValid(String accessToken) async {
@@ -42,15 +78,30 @@ class AuthService {
       },
     );
     if (response.headers['content-type'] == 'application/json') {
-      print(response.statusCode == 200);
       return response.statusCode == 200;
     } else {
-      print('false');
       return false;
     }
   }
 
   Future<void> logout() async {
     await removeToken();
+  }
+
+  Future<String> loadImageFromPreferences() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final imagePath = prefs.getString('profile_picture_path');
+      print('object');
+      print(imagePath);
+      print('object');
+      if (imagePath != null) {
+        return imagePath;
+      } else {
+        return 'assets/images/logo.png';
+      }
+    } catch (e) {
+      return 'assets/images/logo.png';
+    }
   }
 }
