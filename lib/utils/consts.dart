13823f952ab5d95f3a280f3d445ff6986a0eb42a
Amin Hashemi <43804911+aaminhashemi@users.noreply.github.com>
2024-02-09 12:48:33 +0330

    Create consts.dart

---
+++ lib/utils/consts.dart
@@ -0,0 +1,76 @@
+class Consts{
+  static const String login='ورود';
+  static const String loginToApplication='ورود به برنامه';
+  static const String loginUserName='نام کاربری';
+  static const String loginPassword='کلمه عبور';
+
+  static const String update='به روز رسانی';
+  static const String updateIsAvailable='نسخه ی جدیدی از اپلیکیشن در دسترس است!';
+  static const String cancel='لغو';
+  static const String applicationTitle='اپلیکیشن افخم';
+
+  static const String personnelList='لیست پرسنل';
+  static const String personnelPhone='شماره تلفن';
+  static const String personnelEmergencyPhone='شماره تلفن اضطراری';
+  static const String personnelMobile='موبایل';
+  static const String personnelBirthDate='تاریخ تولد';
+  static const String personnelAddress='آدرس';
+  static const String personnelEducationalDegree='مدرک تحصیلی';
+  static const String personnelMaritalStatus='وضعیت تاهل';
+  static const String personnelChildren='تعداد فرزند';
+  static const String personnelCode='کد پرسنلی';
+  static const String personnelEmail='ایمیل';
+
+  static const String payslip='فیش حقوقی';
+  static const String period='دوره';
+  static const String reason='علت';
+  static const String save='ثبت';
+  static const String value='مبلغ';
+  static const String status='وضعیت';
+  static const String create='ایجاد';
+  static const String start='آغاز';
+  static const String end='پایان';
+  static const String priceUnit='ریال';
+  static const String download='دریافت';
+  static const String daily='روزانه';
+  static const String origin='مبدا';
+  static const String year='سال';
+  static const String month='ماه';
+  static const String select='انتخاب';
+  static const String day='روز';
+  static const String destination='مقصد';
+  static const String description='توضیحات';
+  static const String startTime='ساعت آغاز';
+  static const String startDate='تاریخ آغاز';
+  static const String endTime='ساعت پایان';
+  static const String endDate='تاریخ پایان';
+  static const String hourly='ساعتی';
+  static const String paymentDate='تاریخ پرداخت';
+  static const String noPayslipWasFound='تاکنون فیش حقوقی برای شما ثبت نشده است.';
+
+  static const String missionSavedSuccessfully='درخواست ماموریت با موفقیت ثبت شد.';
+  static const String RequestSavedSuccessfully='درخواست با موفقیت ثبت شد.';
+  static const String noMissionsFound='تاکنون درخواست ماموریت ثبت نکرده اید.';
+  static const String noLoansFound='تاکنون درخواست وام ثبت نکرده اید.';
+  static const String pleaseEnterDataCompletely='لطفا اطلاعات را به صورت کامل وارد نمایید.';
+  static const String endTimeCantBeLessThanStartTime='زمان پایان نمی تواند زودتر از زمان شروع باشد.';
+
+  static const String missionRequest='درخواست ماموریت';
+  static const String requestedValue='مبلغ درخواستی';
+  static const String repaymentCount='تعداد اقساط';
+  static const String repaymentValue='مبلغ هر قسط';
+  static const String residueValue='مبلغ باقی مانده';
+  static const String selectStartDate='انتخاب تاریخ شروع';
+  static const String selectEndDate='انتخاب تاریخ پایان';
+  static const String requestDate='تاریخ درخواست';
+  static const String selectMissionType='انتخاب نوع ماموریت';
+  static const String SuperiorDescription='توضیحات سرپرست';
+  static const String missionsList='لیست ماموریت ها';
+  static const String loansList='لیست درخواست های وام';
+  static const String requestedRepaymentCount='تعداد اقساط پیشنهادی';
+  static const String saveLoanRequest='ثبت درخواست وام';
+  static const String loanRequest='درخواست وام';
+  static const String missionType='نوع ماموریت';
+
+
+}
