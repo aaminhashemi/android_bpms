19cb7f4cfc8fdd29071b8bec49199dfce2fd8a7e
Amin Hashemi <43804911+aaminhashemi@users.noreply.github.com>
2024-02-16 08:26:40 +0330

    Create otp_login.dart

---
+++ lib/screens/otp_login.dart
@@ -0,0 +1,293 @@
+import 'dart:typed_data';
+import 'dart:io';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../services/auth_service.dart';
+import '../services/update_service.dart';
+import '../utils/custom_notification.dart';
+import '../utils/exception_consts.dart';
+import '../utils/custom_color.dart';
+
+class OtpLoginScreen extends StatefulWidget {
+  @override
+  _OtpLoginScreenState createState() => _OtpLoginScreenState();
+}
+
+class _OtpLoginScreenState extends State<OtpLoginScreen> {
+  final AuthService authService = AuthService('https://afkhambpms.ir/api1');
+  final UpdateService updateService =
+      UpdateService('https://afkhambpms.ir/api1/update');
+  TextEditingController mobileController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isLoading = false;
+  File? _imageFile;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _login() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      final loginResponse = await authService.login(
+        mobileController.text.trim(),
+        passwordController.text,
+      );
+
+      if (loginResponse.containsKey('access_token')) {
+        authService.saveToken(loginResponse['access_token']);
+        authService.saveInfo(loginResponse['user'], loginResponse['code']);
+        _fetchImageFromServer(loginResponse['code']);
+        Navigator.pushReplacementNamed(context, '/main');
+      } else {
+        CustomNotification.show(context, 'ناموفق', Exception_consts.incorrectCredentials, '');
+      }
+    } catch (e) {
+      CustomNotification.show(context, 'ناموفق', 'خطا در برقراری ارتباط، اتصال به اینترنت را بررسی نمایید.', '');
+
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _fetchImageFromServer(String code) async {
+    final imageBytes = await authService.fetchImageFromServer();
+    if (imageBytes != null) {
+      _saveImageLocally(imageBytes, code);
+    }
+  }
+
+  Future<void> _saveImageLocally(Uint8List imageBytes, String imageName) async {
+    try {
+      final directory = await getApplicationDocumentsDirectory();
+      final imagePath = '${directory.path}/${imageName}.jpg';
+      await File(imagePath).writeAsBytes(imageBytes);
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('profile_picture_path', imagePath);
+      setState(() {
+        _imageFile = File(imagePath);
+      });
+    } catch (e) {
+      print('Error saving image: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColor.backgroundColor,
+      appBar: AppBar(
+        //title: Text(Consts.loginToApplication),
+        automaticallyImplyLeading: false,
+        backgroundColor: CustomColor.backgroundColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(children: [
+              CircleAvatar(
+                backgroundImage:
+                    AssetImage('assets/images/logo.png') as ImageProvider,
+                radius: 60.0,
+              ),
+              SizedBox(height: 32.0),
+            ]),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius:
+                    BorderRadius.circular(2.0), // Adjust the radius as needed
+              ),
+              //elevation: 8,
+              color: CustomColor.cardColor,
+
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'اطلاعات ورود',
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              color: CustomColor.textColor),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16.0),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 75,
+                          child: Text(
+                            'نام کاربری :',
+                            style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.normal,
+                                color: CustomColor.textColor),
+                          ),
+                        ),
+                        SizedBox(width: 8.0),
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border(
+                                  left: BorderSide(
+                                      color: CustomColor.textColor,
+                                      width: 4.0)),
+                            ),
+                            child: TextField(
+                              controller: mobileController,
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.white,
+                                contentPadding:
+                                    EdgeInsets.symmetric(vertical: 7),
+                                isDense: true,
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16.0),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 75,
+                          child: Text(
+                            'کد تایید :',
+                            style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.normal,
+                                color: CustomColor.textColor),
+                          ),
+                        ),
+                        SizedBox(width: 8.0),
+                        Expanded(
+                            child: Container(
+                          decoration: BoxDecoration(
+                            border: Border(
+                                left: BorderSide(
+                                    color: CustomColor.textColor, width: 4.0)),
+                          ),
+                          child: TextField(
+                            controller: passwordController,
+                            style: TextStyle(),
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              contentPadding: EdgeInsets.symmetric(vertical: 7),
+                              isDense: true,
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        )),
+                      ],
+                    ),
+                    SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: isLoading ? null : _login,
+                      child: isLoading
+                          ? CircularProgressIndicator()
+                          : Text(
+                              'ورود',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              10.0), // Adjust the radius as needed
+                        ),
+                        minimumSize: const Size(double.infinity, 48),
+                        primary: CustomColor.successColor,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 15),
+                          child:
+                        RichText(
+                          text: TextSpan(
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'ورود با کد تایید',
+                                style: TextStyle(fontFamily: 'irs',
+                                    fontSize: 14,
+                                    color: Colors.blue),recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.pushReplacementNamed(context, '/verify-mobile');
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(right: 8),
+                      child: Row(
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'در صورت فراموشی کلمه عبور ',
+                                  style: TextStyle(fontFamily: 'irs',
+                                      fontSize: 14,
+                                      color: CustomColor.textColor),
+                                ),
+                                TextSpan(
+                                  text: 'اینجا تپ ',
+                                  style: TextStyle(fontFamily: 'irs',
+                                      decoration: TextDecoration.underline,
+                                      fontSize: 14,
+                                      color: Colors.red),recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    Navigator.pushReplacementNamed(context, '/verify-mobile');
+                                  },
+                                ),
+                                TextSpan(
+                                  text: 'کنید.',
+                                  style: TextStyle(fontFamily: 'irs',
+                                      fontSize: 14,
+                                      color: CustomColor.textColor),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ))
+                ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
