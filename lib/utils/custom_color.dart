7a2891f66be60ce6f55ab71f6b1ed6a7da67e144
Amin Hashemi <43804911+aaminhashemi@users.noreply.github.com>
2024-02-16 08:26:44 +0330

    Update custom_color.dart

--- lib/utils/custom_color.dart
+++ lib/utils/custom_color.dart
@@ -6,11 +6,11 @@ class CustomColor{
   static const Color dangerColor = Color(0xfff43f5e);
   static const Color emptyListColor = Color(0xfffee2e2);
   static const Color warningColor = Color(0xfffde047);
-  static const Color buttonColor = Color(0xff4ade80);
-  static const Color textColor = Color(0xff1e293b);
+  static const Color buttonColor = Color(0xff369946);
+  static const Color textColor = Color(0xff016737);
   static const Color borderColor = Color(0xff4ade80);
   //static const Color cardColor = Color(0xFF8CF6C2);
-  static const Color cardColor = Color(0xfffeea6f);
+  static const Color cardColor = Color(0xfffff39f);
   static const Color test1 = Color(0xFF5CDB95);
   static const Color backgroundColor = Color(0xffffda00);
   static const Color drawerBackgroundColor = Color(0xff006738);
